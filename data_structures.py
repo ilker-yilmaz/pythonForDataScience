80b5970b6a7883e0fc44079bb4c4ea368f60ec5e
ilker yilmaz <ilkery549@gmail.com>
2022-06-25 15:38:49 +0300

    Create data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,62 @@
+#########################################
+# Data Structures
+#########################################
+# - Veri Yapılarına Giriş ve Hızlı Özet
+# - Sayılar (Numbers): int, float, complex
+# - Karakter Dizileri (Strings): str
+# - List, Dictionary, Tuple, Set, Boolean (TRUE-FALSE): bool
+# - list: liste
+# - dictionary: sözlük
+# - tuple: demet
+# - set: küme
+
+#########################################
+# Veri Yapılarına Giriş ve Hızlı Özet
+#########################################
+
+# sayılar (Numbers): int, float, complex
+y = 46
+print(type(y))
+
+#sayılar: float
+x=10.3
+type(x)
+
+#sayılar: complex
+x=2j+1
+type(x)
+
+#string
+a="Hello ai era"
+type(a)
+
+#boolean
+True
+False
+type(True)
+5==4
+3==2
+1==1
+type(3==2)
+
+#Liste (list)
+
+x=["btc", "eth", "xrp"]
+type(x)
+
+#sözlük (dictionary)
+x={"name":"Peter", "Age":36}
+type(x)
+
+# Tuple (demet)
+x=("python", "ml", "ds")
+type(x)
+
+# Set (küme)
+x={"python", "ml", "ds"}
+type(x)
+
+#Not: Liste, tuple, set ve dictionary veri yapıları aynı zamanda Python Collections (Arrays) olarak geçmektedir.
+
+
+
