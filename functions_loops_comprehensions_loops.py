5fe9502600e9b7b8e234593609cc4975331fa875
ilker yilmaz <ilkery549@gmail.com>
2022-06-28 18:26:42 +0300

    Update functions_loops_comprehensions_loops.py

--- functions_loops_comprehensions_loops.py
+++ functions_loops_comprehensions_loops.py
@@ -198,3 +198,19 @@ def all_calculation(varm, moisture, charge, p):
 
 all_calculation(76, 100, 67, 100)
 
+#######################################################
+# Lokal & Global Değişkenler (Local & Global Variables)
+#######################################################
+
+list_store = [1, 2]
+
+
+def add_element(a, b):
+    c = a * b
+    list_store.append(c)
+    print(list_store)
+
+
+add_element(10, 20)
+
+
